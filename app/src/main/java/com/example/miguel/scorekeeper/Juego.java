418a110039d5246e3873c8509b84ade89dfabef9
Miguel Encarnacion <miguelencarnacion7@gmail.com>
2015-04-02 21:25:01 -0400

    clases Juego y Equipo creadas

---
+++ app/src/main/java/com/example/miguel/scorekeeper/Juego.java
@@ -0,0 +1,52 @@
+package com.example.miguel.scorekeeper;
+
+/**
+ * Created by miguel on 04/02/15.
+ */
+public class Juego {
+    private String nombre;
+    private String descripcion;
+
+    public Juego(){}
+
+    public Juego(String nombre, String descripcion){
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+    }
+
+    /**
+     * Sets nombre.
+     *
+     * @param  nombre
+     */
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+
+    /**
+     * Gets the nombre.
+     *
+     * @return nombre
+     */
+    public String getNombre(){
+        return this.nombre;
+    }
+
+    /**
+     * Sets descripcion.
+     *
+     * @param  descripcion
+     */
+    public void setDescripcion(String descripcion){
+        this.descripcion = descripcion;
+    }
+
+    /**
+     * Gets the descripcion.
+     *
+     * @return descripcion
+     */
+    public String getDescripcion(){
+        return this.descripcion;
+    }
+}
