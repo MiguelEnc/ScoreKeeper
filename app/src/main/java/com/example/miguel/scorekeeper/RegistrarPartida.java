c764a31b01c688d9559175b9efc3e0c68b0b2584
Miguel Encarnacion <miguelencarnacion7@gmail.com>
2015-04-03 16:16:50 -0400

    avance menor en RegistrarPartida

--- app/src/main/java/com/example/miguel/scorekeeper/RegistrarPartida.java
+++ app/src/main/java/com/example/miguel/scorekeeper/RegistrarPartida.java
@@ -2,20 +2,24 @@ package com.example.miguel.scorekeeper;
 
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
 
 
 /**
- * A simple {@link Fragment} subclass.
+ * Created by miguel on 04/03/15.
  */
 public class RegistrarPartida extends android.support.v4.app.Fragment {
 
     private static final String ARG_SECTION_NUMBER = "section_number";
-
+    Context context = this.getActivity();
 
     public RegistrarPartida() {
         // Required empty public constructor
@@ -25,14 +29,33 @@ public class RegistrarPartida extends android.support.v4.app.Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+
+        View v = inflater.inflate(R.layout.fragment_registrar_partida, container, false);
+
+        Button buttonIniciar = (Button) v.findViewById(R.id.buttonIniciarPartida);
+        Spinner spinnerEquipoA = (Spinner) v.findViewById(R.id.spinnerEquipoA);
+        Spinner spinnerEquipoB = (Spinner) v.findViewById(R.id.spinnerEquipoB);
+
+        //Coneccion a la base de datos
+        DBConnection connection = new DBConnection(context);
+
+        //arrayAdapter para popular el Spinner
+        //ArrayAdapter<String> listaJuegos = datosJuegos(connection);
 
         //Aqui se cambia el titulo del fragment
         ((MainActivity) getActivity())
                 .setActionBarTitle(getString(R.string.title_section4));
 
-        return inflater.inflate(R.layout.fragment_registrar_partida, container, false);
+        return v;
     }
+
+    /*
+    private ArrayAdapter<String> datosJuegos(DBConnection connection){
+        ArrayAdapter<String> lista = new ArrayAdapter<String>();
+
+        return lista;
+    }
+    */
 
     @Override
     public void onAttach(Activity activity) {
